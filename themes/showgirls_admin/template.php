2ddfb8f94a720db447ac55486ae7ead03ce3b6e9
ArialBlack <andreyturik@gmail.com>
2016-01-06 23:13:10 +0200

    add custom module

--- themes/showgirls_admin/template.php
+++ themes/showgirls_admin/template.php
@@ -3,3 +3,66 @@
  * @file
  * The primary PHP file for this theme.
  */
+
+
+/**
+ * Override of theme_admin_block_content().
+ */
+function showgirls_admin_admin_block_content($vars) {
+  $content = $vars['content'];
+
+  $output = '';
+  if (!empty($content)) {
+
+    foreach ($content as $k => $item) {
+
+      //-- Safety check for invalid clients of the function
+      if (empty($content[$k]['localized_options']['attributes']['class'])) {
+        $content[$k]['localized_options']['attributes']['class'] = array();
+      }
+      if (!is_array($content[$k]['localized_options']['attributes']['class'])) {
+        $content[$k]['localized_options']['attributes']['class'] = array($content[$k]['localized_options']['attributes']['class']);
+      }
+
+      $content[$k]['title'] = "<span class='icon'></span>" . filter_xss_admin($item['title']);
+      $content[$k]['localized_options']['html'] = TRUE;
+      if (!empty($content[$k]['localized_options']['attributes']['class'])) {
+        $content[$k]['localized_options']['attributes']['class'] += _rubik_icon_classes($item['href']);
+      }
+      else {
+        $content[$k]['localized_options']['attributes']['class'] = _rubik_icon_classes($item['href']);
+      }
+    }
+    $output = system_admin_compact_mode() ? '<ul class="admin-list admin-list-compact">' : '<ul class="admin-list">';
+    foreach ($content as $item) {
+      $output .= '<li class="leaf">';
+      $output .= l($item['title'], $item['href'], $item['localized_options']);
+      if (isset($item['description']) && !system_admin_compact_mode()) {
+        $output .= "<div class='description'>{$item['description']}</div>";
+      }
+      $output .= '</li>';
+    }
+    $output .= '</ul>';
+  }
+  return $output;
+}
+
+
+/**
+ * Generate an icon class from a path.
+ */
+function _rubik_icon_classes($path) {
+  $classes = array();
+  $args = explode('/', $path);
+  if ($args[0] === 'admin' || (count($args) > 1 && $args[0] === 'node' && $args[1] === 'add')) {
+    // Add a class specifically for the current path that allows non-cascading
+    // style targeting.
+    $classes[] = 'path-'. str_replace('/', '-', implode('/', $args)) . '-';
+    while (count($args)) {
+      $classes[] = drupal_html_class('path-'. str_replace('/', '-', implode('/', $args)));
+      array_pop($args);
+    }
+    return $classes;
+  }
+  return array();
+}
